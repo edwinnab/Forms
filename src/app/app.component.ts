ead90a266c9fbcc813a451f73ed8d63940da5b98
edwinnab <edwinnabikeri@gmail.com>
2021-11-11 15:52:03 +0300

    reactive Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { forbiddenNameValidator } from './shared/username.validator';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Form';
+
+  get userName(){
+    return this.registrationForm.get('userName');
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  registrationForm = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator]],
+    email: [''],
+    subscribe: [false],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state: [''],
+      postalCode: ['']
+    })
+  });
+
+  /*registrationForm = new FormGroup({
+    userName: new FormControl('Edwinna'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl('')
+
+    })
+  });
+  */
+
+  loadApiData() {
+    this.registrationForm.patchValue({
+      userName: "Bruce",
+      password: "test",
+      confirmPassword: "test"
+    });
+  }
 }
